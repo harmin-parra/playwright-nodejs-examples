a87f0b654143ea383b12d79658e4bd169ae48e87
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-02-05 22:10:58 +0100

    Add connectors tests

---
+++ tests/2_advanced_examples/connectors.spec.ts
@@ -0,0 +1,78 @@
+import { test, expect } from '@playwright/test';
+const assert = require('node:assert');
+
+test.describe('Connectors', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://example.cypress.io/commands/connectors");
+  });
+
+
+  test('each() - iterate over an array of elements', async({ page }) => {
+    var elms = page.locator(".connectors-its-ul>li");
+    for (var i = 0; i != await elms.count(); i++)
+      console.log(await elms.nth(i).textContent());
+  });
+
+
+  test('get properties on the current subject', async({ page }) => {
+    assert(await page.locator(".connectors-its-ul>li").count() > 2);
+  });
+
+
+  test('evaluate() - invoke a function on the current subject', async({ page }) => {
+    var elem = page.locator(".connectors-div")
+    expect(elem).not.toBeVisible();
+    await elem.evaluate(elem => elem.style.display = 'initial');
+    expect(elem).toBeVisible();
+  });
+
+
+  test('', async({ page }) => {
+    var arr: string[] = ['foo', 'bar', 'baz'];
+
+    function func(foo, bar, baz) {
+        assert(foo == 'foo');
+        assert(bar == 'bar');
+        assert(baz == 'baz');
+    }
+    func(...arr);
+  });
+
+
+  test('invokes a callback function with the current subject', async({ page }) => {
+    var elms = page.locator(".connectors-list > li");
+    assert(await elms.count() == 3, "3 items");
+    expect(elms).toHaveCount(3);
+    expect(elms.nth(0)).toContainText("Walk the dog");
+    expect(elms.nth(1)).toContainText("Feed the cat");
+    expect(elms.nth(2)).toContainText("Write JavaScript");
+  });
+
+
+  test('yields the returned value to the next command', async({ page }) => {
+    function num(x: number) {
+      assert(x == 1);
+      return 2;
+    }
+    assert(num(1) == 2);
+  });
+
+
+  test('yields the original subject without return', async({ page }) => {
+    function num(x: number) {
+      assert(x == 1);
+    }
+    num(1);
+  });
+
+
+  test('yields the value yielded by the last Cypress command inside', async({ page }) => {
+    function num(x: number) {
+      assert(x == 1);
+      return 2;
+    }
+    assert(num(1) == 2);
+  });
+
+});
